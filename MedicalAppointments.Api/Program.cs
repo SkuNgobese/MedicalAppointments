e1e552e4682c63cf85884dce0e09a3f915ebc18a
Innocent Ngobese <innocentn@flowcentric.com>
2025-04-01 22:45:12 +0200

    #

--- MedicalAppointments.Api/Program.cs
+++ MedicalAppointments.Api/Program.cs
@@ -94,7 +94,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Medical Appointments API", Version = "v1" });
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MedicalAppointments API",
+        Version = "v1"
+    });
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -102,12 +106,12 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "MedicalAppointments API v1");
     });
 }
 
